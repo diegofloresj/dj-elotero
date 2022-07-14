10fb0ae45b1d5a6584d7c0432022e51f8f50e3ee
Diego floresj <dfj270801@gmail.com>
2022-07-14 12:48:56 -0500

    update token

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     app: {
         px: '>',
-        token: 'OTg3NDAxMTU2NzU2MTE5NjUy.GSv-qR.4t-UJ0AH3tYz7x-XVXChP6j0elbXS7HrToGS4U',
+        token: 'OTg3NDAxMTU2NzU2MTE5NjUy.G0cb4_.2vk4qD7kZNCmE_Mrz8zes3aInc_U1_Wkefvz70',
         playing: 'DJ Elotero'
     },
 
